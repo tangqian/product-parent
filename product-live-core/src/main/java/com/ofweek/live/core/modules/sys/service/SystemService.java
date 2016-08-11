107a075b8d026466138e11e9cc11273abb020a6e
Administrator <Administrator@QH-20150816EGOQ>
2016-08-11 10:24:18 +0800

    初始搭建框架

---
+++ product-live-core/src/main/java/com/ofweek/live/core/modules/sys/service/SystemService.java
@@ -0,0 +1,199 @@
+/**
+ * Copyright &copy; 2012-2014 <a href="https://github.com/thinkgem/jeesite">JeeSite</a> All rights reserved.
+ */
+package com.ofweek.live.core.modules.sys.service;
+
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+
+import org.apache.shiro.session.Session;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.ofweek.live.core.common.config.Global;
+import com.ofweek.live.core.common.persistence.Page;
+import com.ofweek.live.core.common.security.Digests;
+import com.ofweek.live.core.common.security.shiro.session.SessionDAO;
+import com.ofweek.live.core.common.service.BaseService;
+import com.ofweek.live.core.common.utils.Encodes;
+import com.ofweek.live.core.common.utils.StringUtils;
+import com.ofweek.live.core.common.web.Servlets;
+import com.ofweek.live.core.modules.sys.dao.UserDao;
+import com.ofweek.live.core.modules.sys.entity.User;
+import com.ofweek.live.core.modules.sys.security.SystemAuthorizingRealm;
+import com.ofweek.live.core.modules.sys.utils.UserUtils;
+
+/**
+ * 系统管理，安全相关实体的管理类,包括用户、角色、菜单.
+ * @author ThinkGem
+ * @version 2013-12-05
+ */
+@Service
+@Transactional(readOnly = true)
+public class SystemService extends BaseService implements InitializingBean {
+	
+	public static final String HASH_ALGORITHM = "SHA-1";
+	public static final int HASH_INTERATIONS = 1024;
+	public static final int SALT_SIZE = 8;
+	
+	@Autowired
+	private UserDao userDao;
+	@Autowired
+	private SessionDAO sessionDao;
+	@Autowired
+	private SystemAuthorizingRealm systemRealm;
+	
+	public SessionDAO getSessionDao() {
+		return sessionDao;
+	}
+
+	//-- User Service --//
+	
+	/**
+	 * 获取用户
+	 * @param id
+	 * @return
+	 */
+	public User getUser(String id) {
+		return UserUtils.get(id);
+	}
+
+	/**
+	 * 根据登录名获取用户
+	 * @param loginName
+	 * @return
+	 */
+	public User getUserByLoginName(String loginName) {
+		return UserUtils.getByLoginName(loginName);
+	}
+	
+	public Page<User> findUser(Page<User> page, User user) {
+		// 设置分页参数
+		user.setPage(page);
+		// 执行分页查询
+		page.setList(userDao.findList(user));
+		return page;
+	}
+	
+	/**
+	 * 无分页查询人员列表
+	 * @param user
+	 * @return
+	 */
+	public List<User> findUser(User user){
+		List<User> list = userDao.findList(user);
+		return list;
+	}
+	
+	@Transactional(readOnly = false)
+	public void saveUser(User user) {
+		if (StringUtils.isBlank(user.getId())){
+			user.preInsert();
+			userDao.insert(user);
+		}else{
+			// 更新用户数据
+			user.preUpdate();
+			userDao.update(user);
+		}
+		if (StringUtils.isNotBlank(user.getId())){
+			// 更新用户与角色关联
+			userDao.deleteUserRole(user);
+			// 清除用户缓存
+			UserUtils.clearCache(user);
+//			// 清除权限缓存
+//			systemRealm.clearAllCachedAuthorizationInfo();
+		}
+	}
+	
+	@Transactional(readOnly = false)
+	public void updateUserInfo(User user) {
+		user.preUpdate();
+		userDao.updateUserInfo(user);
+		// 清除用户缓存
+		UserUtils.clearCache(user);
+//		// 清除权限缓存
+//		systemRealm.clearAllCachedAuthorizationInfo();
+	}
+	
+	@Transactional(readOnly = false)
+	public void deleteUser(User user) {
+		userDao.delete(user);
+		// 清除用户缓存
+		UserUtils.clearCache(user);
+//		// 清除权限缓存
+//		systemRealm.clearAllCachedAuthorizationInfo();
+	}
+	
+	@Transactional(readOnly = false)
+	public void updatePasswordById(String id, String loginName, String newPassword) {
+		User user = new User(id);
+		user.setPassword(entryptPassword(newPassword));
+		userDao.updatePasswordById(user);
+		// 清除用户缓存
+		user.setLoginName(loginName);
+		UserUtils.clearCache(user);
+//		// 清除权限缓存
+//		systemRealm.clearAllCachedAuthorizationInfo();
+	}
+	
+	@Transactional(readOnly = false)
+	public void updateUserLoginInfo(User user) {
+		// 保存上次登录信息
+		user.setOldLoginIp(user.getLoginIp());
+		user.setOldLoginDate(user.getLoginDate());
+		// 更新本次登录信息
+		user.setLoginIp(StringUtils.getRemoteAddr(Servlets.getRequest()));
+		user.setLoginDate(new Date());
+		userDao.updateLoginInfo(user);
+	}
+	
+	/**
+	 * 生成安全的密码，生成随机的16位salt并经过1024次 sha-1 hash
+	 */
+	public static String entryptPassword(String plainPassword) {
+		String plain = Encodes.unescapeHtml(plainPassword);
+		byte[] salt = Digests.generateSalt(SALT_SIZE);
+		byte[] hashPassword = Digests.sha1(plain.getBytes(), salt, HASH_INTERATIONS);
+		return Encodes.encodeHex(salt)+Encodes.encodeHex(hashPassword);
+	}
+	
+	/**
+	 * 验证密码
+	 * @param plainPassword 明文密码
+	 * @param password 密文密码
+	 * @return 验证成功返回true
+	 */
+	public static boolean validatePassword(String plainPassword, String password) {
+		String plain = Encodes.unescapeHtml(plainPassword);
+		byte[] salt = Encodes.decodeHex(password.substring(0,16));
+		byte[] hashPassword = Digests.sha1(plain.getBytes(), salt, HASH_INTERATIONS);
+		return password.equals(Encodes.encodeHex(salt)+Encodes.encodeHex(hashPassword));
+	}
+	
+	/**
+	 * 获得活动会话
+	 * @return
+	 */
+	public Collection<Session> getActiveSessions(){
+		return sessionDao.getActiveSessions(false);
+	}
+	
+	/**
+	 * 获取Key加载信息
+	 */
+	public static boolean printKeyLoadMessage(){
+		StringBuilder sb = new StringBuilder();
+		sb.append("\r\n======================================================================\r\n");
+		sb.append("\r\n    欢迎使用 "+Global.getConfig("productName")+"  - Powered By http://www.ofweek.com\r\n");
+		sb.append("\r\n======================================================================\r\n");
+		System.out.println(sb.toString());
+		return true;
+	}
+	
+	public void afterPropertiesSet() throws Exception {
+	}
+	
+}
---
+++ product-live-core/src/main/java/com/ofweek/live/core/modules/sys/utils/UserUtils.java
@@ -0,0 +1,173 @@
+/**
+ * Copyright &copy; 2012-2014 <a href="https://github.com/thinkgem/jeesite">JeeSite</a> All rights reserved.
+ */
+package com.ofweek.live.core.modules.sys.utils;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.UnavailableSecurityManagerException;
+import org.apache.shiro.session.InvalidSessionException;
+import org.apache.shiro.session.Session;
+import org.apache.shiro.subject.Subject;
+
+import com.ofweek.live.core.common.utils.CacheUtils;
+import com.ofweek.live.core.common.utils.SpringContextHolder;
+import com.ofweek.live.core.modules.sys.dao.UserDao;
+import com.ofweek.live.core.modules.sys.entity.User;
+import com.ofweek.live.core.modules.sys.security.SystemAuthorizingRealm.Principal;
+
+/**
+ * 用户工具类
+ * @author ThinkGem
+ * @version 2013-12-05
+ */
+public class UserUtils {
+
+	private static UserDao userDao = SpringContextHolder.getBean(UserDao.class);
+	public static final String USER_CACHE = "userCache";
+	public static final String USER_CACHE_ID_ = "id_";
+	public static final String USER_CACHE_LOGIN_NAME_ = "ln";
+	
+	/**
+	 * 根据ID获取用户
+	 * @param id
+	 * @return 取不到返回null
+	 */
+	public static User get(String id){
+		User user = (User)CacheUtils.get(USER_CACHE, USER_CACHE_ID_ + id);
+		if (user ==  null){
+			user = userDao.get(id);
+			if (user == null){
+				return null;
+			}
+			CacheUtils.put(USER_CACHE, USER_CACHE_ID_ + user.getId(), user);
+			CacheUtils.put(USER_CACHE, USER_CACHE_LOGIN_NAME_ + user.getLoginName(), user);
+		}
+		return user;
+	}
+	
+	/**
+	 * 根据登录名获取用户
+	 * @param loginName
+	 * @return 取不到返回null
+	 */
+	public static User getByLoginName(String loginName){
+		User user = (User)CacheUtils.get(USER_CACHE, USER_CACHE_LOGIN_NAME_ + loginName);
+		if (user == null){
+			user = userDao.getByLoginName(new User(null, loginName));
+			if (user == null){
+				return null;
+			}
+			CacheUtils.put(USER_CACHE, USER_CACHE_ID_ + user.getId(), user);
+			CacheUtils.put(USER_CACHE, USER_CACHE_LOGIN_NAME_ + user.getLoginName(), user);
+		}
+		return user;
+	}
+	
+	/**
+	 * 清除当前用户缓存
+	 */
+	public static void clearCache(){
+		UserUtils.clearCache(getUser());
+	}
+	
+	/**
+	 * 清除指定用户缓存
+	 * @param user
+	 */
+	public static void clearCache(User user){
+		CacheUtils.remove(USER_CACHE, USER_CACHE_ID_ + user.getId());
+		CacheUtils.remove(USER_CACHE, USER_CACHE_LOGIN_NAME_ + user.getLoginName());
+		CacheUtils.remove(USER_CACHE, USER_CACHE_LOGIN_NAME_ + user.getOldLoginName());
+	}
+	
+	/**
+	 * 获取当前用户
+	 * @return 取不到返回 new User()
+	 */
+	public static User getUser(){
+		Principal principal = getPrincipal();
+		if (principal!=null){
+			User user = get(principal.getId());
+			if (user != null){
+				return user;
+			}
+			return new User();
+		}
+		// 如果没有登录，则返回实例化空的User对象。
+		return new User();
+	}
+	
+	/**
+	 * 获取授权主要对象
+	 */
+	public static Subject getSubject(){
+		return SecurityUtils.getSubject();
+	}
+	
+	/**
+	 * 获取当前登录者对象
+	 */
+	public static Principal getPrincipal(){
+		try{
+			Subject subject = SecurityUtils.getSubject();
+			Principal principal = (Principal)subject.getPrincipal();
+			if (principal != null){
+				return principal;
+			}
+//			subject.logout();
+		}catch (UnavailableSecurityManagerException e) {
+			
+		}catch (InvalidSessionException e){
+			
+		}
+		return null;
+	}
+	
+	public static Session getSession(){
+		try{
+			Subject subject = SecurityUtils.getSubject();
+			Session session = subject.getSession(false);
+			if (session == null){
+				session = subject.getSession();
+			}
+			if (session != null){
+				return session;
+			}
+//			subject.logout();
+		}catch (InvalidSessionException e){
+			
+		}
+		return null;
+	}
+	
+	// ============== User Cache ==============
+	
+	public static Object getCache(String key) {
+		return getCache(key, null);
+	}
+	
+	public static Object getCache(String key, Object defaultValue) {
+//		Object obj = getCacheMap().get(key);
+		Object obj = getSession().getAttribute(key);
+		return obj==null?defaultValue:obj;
+	}
+
+	public static void putCache(String key, Object value) {
+//		getCacheMap().put(key, value);
+		getSession().setAttribute(key, value);
+	}
+
+	public static void removeCache(String key) {
+//		getCacheMap().remove(key);
+		getSession().removeAttribute(key);
+	}
+	
+//	public static Map<String, Object> getCacheMap(){
+//		Principal principal = getPrincipal();
+//		if(principal!=null){
+//			return principal.getCacheMap();
+//		}
+//		return new HashMap<String, Object>();
+//	}
+	
+}
